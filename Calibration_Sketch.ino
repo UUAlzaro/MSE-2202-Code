ac30b86b40cc622cfc1043b65cb04e984cd87f87
UUAlzaro <156833234+UUAlzaro@users.noreply.github.com>
2024-04-01 20:36:26 -0400

    Add files via upload

---
+++ Calibration_Sketch.ino
@@ -0,0 +1,113 @@
+// This is the Calibration sketch! 
+
+//The device outputs a square wave whose frequency changes in correspondence to the intensity of light.
+// For our case, we will use an intensity of 20% (S0 is HIGH, S1 is LOW) Note that Pins S0 and S1 are responsible for scaling the frequencies! 
+
+// Define color sensor pins
+// Pin layout : 
+// 5v to VCC, to the S2,S3 side 
+// GND to ground (again S2,S3 side)
+#define S0 4 // pin 4 in my arduino 
+#define S1 5
+#define S2 6
+#define S3 7
+#define sensorOut 8
+
+// Variables for Color Pulse Width Measurements
+
+int redPW = 0;
+int greenPW = 0;
+int bluePW = 0;
+
+void setup() {
+
+  // Set S0 - S3 as outputs
+  pinMode(S0, OUTPUT);
+  pinMode(S1, OUTPUT);
+  pinMode(S2, OUTPUT);
+  pinMode(S3, OUTPUT);
+  pinMode(12,OUTPUT); // pin 12 in my ardunio 
+  
+  // Set Sensor output as input
+  pinMode(sensorOut, INPUT);
+  
+  // Set Pulse Width scaling to 20%
+  digitalWrite(S0,HIGH);
+  digitalWrite(S1,LOW);
+  
+  // Setup Serial Monitor
+  Serial.begin(9600);
+}
+
+void loop() {
+  digitalWrite(12,HIGH); 
+  // Read Red Pulse Width
+  redPW = getRedPW();
+  // Delay to stabilize sensor
+  delay(200);
+  
+  // Read Green Pulse Width
+  greenPW = getGreenPW();
+  // Delay to stabilize sensor
+  delay(200);
+  
+  // Read Blue Pulse Width
+  bluePW = getBluePW();
+  // Delay to stabilize sensor
+  delay(200);
+  
+  // Print output to Serial Monitor
+  Serial.print("Red PW = ");
+  Serial.print(redPW);
+  Serial.print(" - Green PW = ");
+  Serial.print(greenPW);
+  Serial.print(" - Blue PW = ");
+  Serial.println(bluePW);
+  
+}
+
+
+// Function to read Red Pulse Widths
+int getRedPW() {
+
+  // Set sensor to read Red only
+  digitalWrite(S2,LOW); // setting S2 and S3 to low is code for saying "only red the amount of red". 
+  digitalWrite(S3,LOW);
+  // Define integer to represent Pulse Width
+  int PW;
+  // Read the output Pulse Width
+  PW = pulseIn(sensorOut, LOW);
+  // Return the value
+  return PW;
+
+}
+
+// Function to read Green Pulse Widths
+int getGreenPW() {
+
+  // Set sensor to read Green only
+  digitalWrite(S2,HIGH);
+  digitalWrite(S3,HIGH);
+  // Define integer to represent Pulse Width
+  int PW;
+  // Read the output Pulse Width
+  PW = pulseIn(sensorOut, LOW);
+  // Return the value
+  return PW;
+
+}
+
+// Function to read Blue Pulse Widths
+int getBluePW() {
+
+  // Set sensor to read Blue only
+  digitalWrite(S2,LOW);
+  digitalWrite(S3,HIGH);
+  // Define integer to represent Pulse Width
+  int PW;
+  // Read the output Pulse Width
+  PW = pulseIn(sensorOut, LOW);
+  // Return the value
+  return PW;
+
+}
